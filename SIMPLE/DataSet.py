4e58f090f63614cf876f7704b4128d89462784dc
Juliana Gomes <juliana.m.l.gomes@gmail.com>
2014-01-31 00:58:24 +0000

    adding simple

---
+++ SIMPLE/DataSet.py
@@ -0,0 +1,412 @@
+from sys import stdout
+import numpy as np
+import re
+import math
+import copy
+import subprocess
+from Test import *
+
+
+#EVAL_TOOL CONSTANTS
+SCRIPT_DATASET ="Eval-Score-4.0.pl"    # eval-score-mslr.pl for OSHUMED, Eval-Score-4.0.pl for MQ2008
+RESULTS_P_QUERY = "0" #to return results averaged by query assign 0, otherwise assign 1
+
+
+class DataSet:
+    """
+
+        This class is the representation of a DataSet
+        The DataSet is in feature_Vectors plus id_query+id_doc and score
+        contains all the operations to learn to rank with GA
+
+    """
+
+    isTest = False #True if the dataset is Test, False if it is Train
+    DCG = 0 #Discounted cumulative Gain of a particular order
+    perfectDCG = -1 #DCG of the perfect order
+    FeaturesNum = 0 # Number of Features of a particular dataSet
+    dataType = None #Numpy dataType of the structure of dataSet
+    dataSet = None #the representation of the DataSet in features vector
+    currentChromosome = None #the current chromosome
+    BestIndividual = None #the best individual after all generations of GA
+    DocCounter = 0 #a counter to count the # of docs of dataset
+    
+    MutationRate = None
+    PopulationSize = None
+    Generations = None
+
+    DataSetFileName = None #typically test.txt or train.txt
+
+    def __init__(self, isT, data):
+        """
+         the class constructor creates the dataset with its features vectors
+        """
+        self.isTest = isT
+        lineNumber = 1
+        self.DocCounter = 0
+        for queryDoc in data:
+            if lineNumber==1:
+                self.set_features_number(queryDoc)
+                self.calc_data_type()
+                self.dataSet = np.recarray((1,),self.dataType)
+                self.dataSet[0] = self.parse_query_doc(queryDoc,lineNumber)
+            else:
+                self.dataSet = np.append(self.dataSet, self.parse_query_doc(queryDoc,lineNumber))
+            lineNumber+=1
+
+
+    def calc_data_type(self):
+        """
+            creates the dtype of the numpy array
+        """
+        self.dataType = [('query','i4'),('doc','S50'),('rel','i4'),('lineNum','i4'),('score','i4')]
+        i = 1
+        while i <= self.FeaturesNum:
+            self.dataType.append(('f-'+str(i),'float64'))
+            i+=1
+
+
+    def get_doc_for_line(self,line):
+        """
+            returns the doc id
+        """
+        doc = line.index('#docid = ')
+        doc_and_after = line[doc+9:]
+        try:
+            Space_i = doc_and_after.index(' ')
+            doc_and_after =  doc_and_after[:Space_i]
+
+            return doc_and_after
+        except:
+            doc_and_after.replace('\n','')
+            doc_and_after.replace('\r','')
+            return doc_and_after
+
+
+    def get_query_id(self,line):
+        """
+            returns the query id
+        """
+        try:
+            qid = line.index('qid:')
+            query_id = line[qid+4:]
+            space = query_id.index(' ')
+            return query_id[:space]
+        except:
+            return ""
+
+
+    def parse_query_doc(self, line, lineNum):
+        """
+            parses a line of the training files
+        """
+        self.DocCounter += 1
+        doc =[]
+        score  = int(line[0])
+        query = self.get_query_id(line)
+        doc.insert(0,query)
+        doc.insert(1,str(self.DocCounter))
+        doc.insert(2,score)
+        doc.insert(3,lineNum)
+        doc.insert(4,-1)
+        fi = 5
+        i=1
+        while(i <= self.FeaturesNum):
+            str_i = str(i)
+            f_index = line.index(str_i+':')
+            offset = len(str_i)+1
+            feature = line[f_index+offset:]
+            space_index = feature.index(' ')
+            feature = feature[: space_index]
+            ffeature = float(feature)
+            doc.insert(fi,ffeature)
+            i+=1
+            fi+=1
+        dataset = np.array(tuple(doc),dtype=self.dataType)
+        return dataset
+    
+
+    def sort_ideal(self):
+        """
+            sorts dataset according relevance
+        """
+        self.dataSet = np.sort(self.dataSet,order=['query','rel'])[::-1]
+        self.__setPerfectDCG__(self.get_ndcg())
+
+
+    def set_features_number(self,line):
+        """
+            calcs the number of features and sets info in dataSet
+        """
+        inv_line = line.split(' ')
+        inv_line = inv_line[::-1]
+        regex = re.compile('^[0-5]*[0-9]+:')
+        for e in inv_line:
+            if regex.match(e):
+                i = e.index(':')
+                e = e[:i]
+                self.FeaturesNum = int(e)
+                break
+
+
+    def get_feature(self,c):
+        """
+            receives the gene and returns the Feature num in dataSet
+        """
+        TotalFeatNum = self.FeaturesNum
+        num = math.fmod(c,TotalFeatNum)
+        num = int(num)
+        if num == 0:
+            num = TotalFeatNum
+        return num
+    
+
+    def remove_duplicate_features(self,chromosome):
+        """
+            receives the chromosome and removes the duplicate features
+        """
+        freeDups = []
+        newChromosome = []
+        for gene in chromosome:
+            feature = self.get_feature(gene)
+            if feature not in freeDups:
+                freeDups.append(feature)
+                newChromosome.append(gene)
+        return newChromosome
+
+
+    def is_ascending(self,c):
+        """
+            receives a gene
+            returns True if gene <= total # of features -> order ascending
+            returns False otherwise -> order descending
+        """
+        if c > (self.FeaturesNum):
+            return False
+        else:
+            return True
+
+
+    def get_order_by_chromosome(self, N=-1):
+        """
+            returns a order to be used by the numpy.lexort method to sort the dataset based on the current chromosome
+        """
+        chromo_order = []
+        chromosomeR = None
+        if N <= 0:
+            chromosomeR = self.currentChromosome[::-1]
+        else:
+            chromosomeR = self.currentChromosome[:N]
+            chromosomeR = chromosomeR[::-1]
+        for c in chromosomeR:
+            f_index = 'f-'
+            gene = int(c)
+            n_feature = self.get_feature(gene)
+            f_index += str(n_feature)
+            if self.is_ascending(gene):
+                chromo_order.append(self.dataSet[f_index])
+            else:
+                chromo_order.append(- self.dataSet[f_index])
+        chromo_order.append(self.dataSet['query'])
+        return chromo_order
+
+
+    def sort_dataset_by_chromosome(self, chromosome):
+        """
+            receives a chromosome and sorts dataset according it
+        """
+        newChromosome = self.remove_duplicate_features(chromosome)
+        self.set_current_chromosome(newChromosome)
+        order = self.get_order_by_chromosome()
+        self.dataSet = self.dataSet[np.lexsort(order)]
+
+
+
+    def complex_sort(self,chromosome):
+        """
+            sorts the dataset step by step ; gene by gene
+            use for debug it is useful to check the differences
+        """
+        i =1
+        newChromosome = self.remove_duplicate_features(chromosome)
+        self.set_current_chromosome(newChromosome)
+        cromo_len = len(newChromosome)
+        oldList = copy.copy(self.dataSet)
+        while i <= cromo_len:
+            current_order = self.get_order_by_chromosome(i)
+            self.dataSet = self.dataSet[np.lexsort(current_order)]
+            Test.checkOrderedList(self)
+            n_diff = self.compare_the_differences(oldList)
+            Test.print_differences_between_lists(self,n_diff,newChromosome[i-1])
+            oldList = copy.copy(self.dataSet)
+            i+=1
+        fich=open('complex_sort.txt','a')
+        fich.write(str(chromosome)+'original chromosome \n')
+        fich.write(str(newChromosome)+' used chromosome \n')
+        fich.close()
+
+
+    def compare_the_differences(self, oldList):
+        """
+            method used for debug
+            returns the # of differences between a previous sort and a current sort
+        """
+        diff = 0
+        self.dataSet = self.assign_scores(self.dataSet)
+        oldList = self.assign_scores(oldList)
+        for el in oldList:
+            arr_aux = np.array(self.dataSet[self.dataSet['lineNum']== el.lineNum], dtype = self.dataType)
+            if arr_aux[0].score != el.score:
+                #DEBUG CODE:
+                if(el.lineNum != arr_aux[0].lineNum):
+                    Test.report_error('compare_the_differences','comparing score elements with different lineNums')
+                elif(el.query != arr_aux[0].query):
+                    Test.report_error('compare_the_differences','comparing score elements with different queries')
+                diff+=1
+        return diff
+
+
+    def assign_scores(self,corpus):
+        """
+            assigns the scores of the dataSet
+            the eval_tools needs to know the scores of dataset
+            to generate the evaluation metrics: NDCG & MAP
+        """
+        rank_pos =1
+        current_q = None
+        for q in corpus:
+            if current_q != q.query:
+                rank_pos = len(corpus[corpus['query'] == q.query])
+                current_q = q.query
+                q.score = rank_pos
+            else:
+                rank_pos -=1
+                q.score = rank_pos
+        return corpus
+
+
+    def get_ndcg(self):
+        """
+            returns the value of NDCG of the current dataSet
+        """
+        DCGacm = 0.0
+        query_counter = 0
+        current_q = -1
+        relevance_scores = None
+        n = 1
+        for qline in self.dataSet:
+            if current_q != qline.query:
+                if relevance_scores != None:
+                    if(len(relevance_scores)< n):
+                        DCGacm += Test.get_ndcg(relevance_scores,len(relevance_scores))
+                    else:
+                        DCGacm += Test.get_ndcg(relevance_scores,n)
+
+                relevance_scores = []
+                current_q = qline.query
+                query_counter = query_counter+1
+                n = 1
+                #if n <= 10: -----> NDCG@10
+            relevance_scores.append(qline.rel)
+            n+=1
+        DCGacm += Test.get_ndcg(relevance_scores,len(relevance_scores))
+        ndcg= ((DCGacm)/float(query_counter))
+        return ndcg
+
+    
+    def write_scores(self,isTrain=False):
+        """
+            writes the scores of the dataset, so eval_tools
+            can generate the evaluation metrics NDCG+MAP
+        """
+        self.assign_scores(self.dataSet)
+        filename = "scores"+self.DataSetFileName
+        current_q = -1
+        rank_pos = 1
+        for q in self.dataSet:
+            if current_q != q.query:
+                rank_pos = len(self.dataSet[self.dataSet['query']==q.query])
+                current_q = q.query
+                q.score = rank_pos
+            else:
+                rank_pos -=1
+                q.score = rank_pos
+        test_set = np.sort(self.dataSet, order=['lineNum'])
+        text_file = open(filename, "w")
+        for qr in test_set:
+            text_file.write(str(qr.score)+'\n')
+        text_file.close()
+        pipe = subprocess.Popen(["perl", SCRIPT_DATASET ,self.DataSetFileName , filename ,"metrics_"+self.DataSetFileName, RESULTS_P_QUERY], stdout = subprocess.PIPE)
+
+    
+    def set_best_individual(self,Best):
+        """
+            sets BestIndividual - chromosome (the best individual is the one selected by GA)
+        """
+        self.BestIndividual = Best
+
+
+    def get_best_individual(self):
+        """
+            gets the BestIndividual property
+        """
+        return self.BestIndividual
+
+
+    def set_current_chromosome(self,c):
+        """
+            sets the current chromosome
+        """
+        self.currentChromosome = c
+
+    
+    def get_current_chromosome(self):
+        """
+            gets the current chromosome
+        """
+        return self.currentChromosome
+
+    def set_mutation_rate(self,mr):
+        """
+            sets the value of mut. rate
+            just to have this info in dataset
+        """
+        self.MutationRate = mr
+
+
+    def get_mutation_rate(self):
+        """
+            gets the current chromosome
+        """
+        return self.MutationRate
+
+    def set_pop_size(self,ps):
+        """
+            sets the value of pop size
+            just to have this info in dataset
+        """
+        self.PopulationSize = ps
+
+
+    def get_pop_size(self):
+        """
+            gets the Population Size
+        """
+        return self.PopulationSize
+
+    def set_generations(self,g):
+        """
+            sets the value of generations
+            just to have this info in dataset
+        """
+        self.Generations = g
+
+
+    def get_generations(self):
+        """
+            gets the # of Generations
+        """
+        return self.Generations
+
+    def set_dataset_filename(self,name):
+        self.DataSetFileName = name
---
+++ SIMPLE/MainGA.py
@@ -0,0 +1,104 @@
+__author__ = 'julianagomes'
+import sys
+print sys.argv # returns: ['param.py']
+import getopt
+from pyevolve import G1DList
+from pyevolve import GSimpleGA
+from DataSet import *
+import copy
+
+
+GENERATIONS = 1 # Number of generations for Genetic Algorithm
+POPULATION = 2  #Number of individuals
+MUTATION_RATE = 0.315 #Probability of Mutation
+CROSS_OVER_RATE =0.8  #Cross-over rate
+SIZE_OF_CHROMOSOMES = 20 #the length of each chromosome = X genes (< val - better performance but few diversity, > val slow run but more diversity ...)
+
+TrainSet = None #dataset of train data
+TrainSetCopy = None #copy of train data dataset
+TestSet = None #dataset of test data
+
+
+
+def eval_func(chromosome):
+    """
+        GA  fitness function
+    """
+    global TrainSet
+    global TrainSetCopy
+    TrainSetCopy = copy.copy(TrainSet)
+    TrainSetCopy.dataSet = copy.copy(TrainSet.dataSet)
+    TrainSetCopy.sort_dataset_by_chromosome(chromosome)
+    currentNdcg = TrainSetCopy.get_ndcg()
+    return currentNdcg
+
+
+def main(argv):
+    global TrainSet
+    global TestSet
+
+    letters = 'i:t'
+    keywords = ['input=','test=']
+    trainfile =''
+    testfile=''
+
+    #run the algorithm by: python MainGA --input=train.txt --test=test.txt
+    try:
+        opts,arg = getopt.getopt(sys.argv[1:],letters,keywords)
+    except getopt.GetoptError:
+        print 'GetoptError: -i <trainfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-i','--input'):
+            trainfile = arg
+        if opt in ('-t','--test'):
+            testfile = arg
+
+    if trainfile:
+        trainF = open(trainfile, "r")
+        TrainSet = DataSet(False,trainF)
+        trainF.close()
+        TrainSet.set_dataset_filename(str(trainfile))
+        TrainSet.set_generations(GENERATIONS)
+        TrainSet.set_pop_size(POPULATION)
+        TrainSet.set_mutation_rate(MUTATION_RATE)
+        genome = G1DList.G1DList(SIZE_OF_CHROMOSOMES)
+        #* 2 to ascending and descending sorts, otherwise you'll have just ascending order
+        genome.setParams(rangemin= 1, rangemax=(TrainSet.FeaturesNum)*2)
+        genome.evaluator.set(eval_func) #change here if you want a different fitness function
+        ga = GSimpleGA.GSimpleGA(genome)
+        ga.setGenerations(GENERATIONS)      #changes the # of generations(default 100)
+        ga.setPopulationSize(POPULATION)    #changes the # of individuals(default 80)
+        #ga.setMutationRate(MUTATION_RATE)   # --> use it when you want to change the Mutation Rate (default 0.02)
+        #ga.setCrossoverRate(CROSS_OVER_RATE) # --> use it when you want to change the Crossover Rate (default 0.8)
+        # #ga.setMultiProcessing(True) # --> please read this: http://pyevolve.sourceforge.net/wordpress/?p=843
+
+        ga.evolve(freq_stats=10)
+
+
+        chromosome = ga.bestIndividual().getInternalList()  # the chromosome selected by GA
+        #print chromosome ---> you can print the chromosome to see what was selected
+        TrainSet.set_best_individual(chromosome)
+        TrainSet.sort_dataset_by_chromosome(chromosome)
+        TrainSet.write_scores(isTrain=True)
+
+
+    else:
+        sys.exit('GA_Algorithm: A train file is required.')
+
+
+    if testfile:
+        testF = open(testfile,'r')
+        TestSet = DataSet(True,testF)
+        testF.close()
+        TestSet.set_dataset_filename(str(testfile))
+        TestSet.set_generations(GENERATIONS)
+        TestSet.set_pop_size(POPULATION)
+        TestSet.set_mutation_rate(MUTATION_RATE)
+        TestSet.isTest = True
+        TestSet.sort_dataset_by_chromosome(TrainSet.get_best_individual()) #order the testset with chromosome found by GA with trainset
+        TestSet.write_scores()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
